3ea30fbafb023ec5ae55c6c5f66b986b3b437c03
Galiakbarov Timur <T.Galiakbarov@modulbank.ru>
2016-07-26 17:32:49 +0500

    test

--- index.php
+++
@@ -1,6 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetPageProperty("keywords", "1");
-$APPLICATION->SetPageProperty("description", "1");
-$APPLICATION->SetTitle("Оценка эффективности научных школ");
-?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
